1bd3003470b02fe2392edc8c22e2c2a959b6707e
marc wimmer <marc@itewimmer.de>
2016-03-04 09:51:19 +0100

    ...

--- machines/tarball_creator/install-update-bundles-job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'for d in $(ls /home/odoo/.vim/bundle); do cd /home/odoo/.vim/bundle/$d; git pull; pwd; git submodule update --init --recursive; chown odoo:odoo . -R; done' > /etc/cron.hourly/update_vimbundles
-echo 'cd /home/odoo/.vim/UltiSnips; git add .; git commit -am autocommit; git pull; git push; chown odoo:odoo . -R; ' > /etc/cron.hourly/update_ultisnipts
-echo "apt-get update; apt-get upgrade -y" > /etc/cron.hourly/apt_upgrade
-chmod a+x /etc/cron.hourly/*
-
