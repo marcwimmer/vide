cd4fe781f6ae1bba8c28ce403010fb9901541231
marc wimmer <marc@itewimmer.de>
2016-08-18 08:25:58 +0200

    ...

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo "Requires 14.04|16.04 as argument"
+    exit -1
+fi
+
 docker-compose up marcvimbuilder
-docker-compose run testvim bash
+docker-compose run testvim$1 bash
