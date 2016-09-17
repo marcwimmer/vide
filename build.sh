dffebfc2d42091f14244ea8d470a2078a240d324
marc wimmer <marc@itewimmer.de>
2016-09-18 00:29:06 +0200

    .......

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-rm tmp/clone/* -Rf
-docker-compose kill
-if [[ "$1" == "DEBUG" ]]; then
-    docker-compose build
-else
-    docker-compose build --no-cache
-fi
-docker-compose up
+set -e
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+for rev in $(ls $DIR/revisions);
+do
+    echo building $rev
+    cp $DIR/revisions/$rev machine/Dockerfile
+
+    rm tmp/clone/* -Rf || true
+    docker-compose kill
+    if [[ "$1" == "DEBUG" ]]; then
+        docker-compose build
+    else
+        docker-compose build --no-cache
+    fi
+    docker-compose up
+
+    cp $DIR/build/marcvim_installer.sh $DIR/build/marcvim_installer_$rev.sh
+
+done
 echo New marcvim installer is under build
