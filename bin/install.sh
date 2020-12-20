a054cc119c9d6cced3d96a20b0e4623c6fb8cac8
Marc Wimmer <marc@itewimmer.de>
2020-12-20 22:10:47 +0100

    install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -5,15 +5,8 @@ echo
 
 echo 
 echo NEOVim must be installed
-
-echo "Replacing current neovim init file with this (ctrl+c to abort)"
-read
-
-mkdir -p ~/.config/nvim
-rm ~/.config/nvim/init.vim 2>/dev/null|| true
-ln -s "$(pwd)/bin/init.vim"  ~/.config/nvim/init.vim
-
-pip3 install -r requirements3.txt
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+pip3 install -r "$DIR/requirements3.txt"
 
 nvim +'PlugInstall --sync' +qa
 python3 ~/.config/vide/bundles/YouCompleteMe/install.py
