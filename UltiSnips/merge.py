8992e4340e8ed432d563b860865c952f7e564ecd
Marc Wimmer <marc@itewimmer.de>
2021-04-12 23:07:37 +0200

    snips

--- UltiSnips/merge.py
+++ UltiSnips/merge.py
@@ -2,10 +2,17 @@
 from pathlib import Path
 import sys
 import os
+import inspect
+import os
+from pathlib import Path
+import inspect
+import os
+from pathlib import Path
+current_dir = Path(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
 collected = {}
 
-for file in Path(os.getcwd()).glob("*.snippets"):
+for file in Path(current_dir).glob("*.snippets"):
     if '_' not in file.name:
         continue
     base = file.name.split("_")[0]
@@ -16,7 +23,7 @@ for base, paths in collected.items():
     content = ""
     for file in paths:
         content += file.read_text() + '\n'
-    file = Path(os.getcwd()) / f"{base}.snippets"
+    file = Path(current_dir) / f"{base}.snippets"
     file.write_text(content)
 
 # use https://github.com/erietz/ultisnips-vscode to convert to vscode
